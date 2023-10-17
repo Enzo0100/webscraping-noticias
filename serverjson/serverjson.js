923c68fd98b1a697d36daeded2e2596a854a85a2
Enzo <enzo.ceravolo@unifesp.br>
2023-10-17 15:20:14 -0300

    upd

--- serverjson/serverjson.js
+++ serverjson/serverjson.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { exec } = require('child_process');
 
 const app = express();
-const PORT = 3000;
+const PORT = 6060;
 
 app.get('/', (req, res) => {
     exec('python3 scraper.py', (error, stdout, stderr) => {
